55910caaf6dc151c3844d693a65ba855867b39d9
Laka Nodia <laka.nodia@gmail.com>
2022-05-24 01:04:50 +0400

    Add image size and type validation

--- js/scriptservice.js
+++ js/scriptservice.js
@@ -1,5 +1,5 @@
 let serviceFromBlock =document.querySelector('#service-form-block');
-let addServiceForm = document.querySelector('#add-service-form');
+let addServiceForm = document.querySelector('#add-service-icon');
 let closeForm = document.querySelector('#close-form');
 let closeServiceButton = document.querySelector('#close-service-button');
 let saveServiceButton = document.querySelector('#save-service-button');
@@ -34,27 +34,26 @@ deleteServiceButton2.addEventListener('click', function(){
     document.querySelector('#service-div-2').remove();
 });
 
-
-
 function addNewPost(){
     let serviceDivElement = document.createElement('div');
     serviceDivElement.classList.add('service-div');
 
-    // დავამატოთ ფოტო იმგ ტეგში
+    //add image on img tag
     let serviceImage = document.createElement('img');
     serviceImage.classList.add('service-img');
     serviceImage.setAttribute('src', `img/${imgInput.files[0].name}`);
   
-    // დავამატოთ ეს ჩაწერილი ტექსტი h2 ტეგებში
+    //add filled title on h2 tag
     let serviceTitle = document.createElement('h2');
     serviceTitle.classList.add('service-title');
     serviceTitle.textContent = titleInput.value;
 
-    // დავამატოთ ეს ჩაწერილი ტექსტი p ტეგებში
+    //add filled description on p tag
     let serviceDescription = document.createElement('p');
     serviceDescription.classList.add('service-describe');
     serviceDescription.textContent = descriptionInput.value;
     
+    // add delete button on service card
     let deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-service-button');
     deleteButton.textContent = 'Delete Service';
@@ -68,13 +67,15 @@ function addNewPost(){
     serviceDivElement.appendChild(deleteButton);
     servicesBlock.appendChild(serviceDivElement);
     
-    // ჩაწერის შემდეგ უნდა გასუფთავდეს
+    // after filling a form it would clear automatically
     resetForm();
     serviceFromBlock.classList.remove('active-post'); 
 }
+// this function resets a form
 function resetForm() {
     titleInput.value = '';
     descriptionInput.value = '';
+    imgInput.value = '';
     let checkBoxDiv = document.querySelector('#checkbox-main-div');
     checkBoxDiv.querySelectorAll('input[type="checkbox"]').forEach(element => {
         element.checked = false;
@@ -87,20 +88,22 @@ function resetForm() {
     resetErrors();
 }
 
+// this function validates form
 function validateForm() {
-     let errors = {};
-     // ვალიდაცია სერვისის სახელზე მაქსიმუმ 25 სიმბოლო
-     let serviceTitle = document.querySelector('#service-title').value;
-     if (serviceTitle.length>25 || serviceTitle==''){
-         errors.serviceTitle = 'Title can not be more than 25 symbols';
-     }
+    let errors = {};
+     // validation in the name of the service is a maximum of 25 characters
+    let serviceTitle = document.querySelector('#service-title').value;
+    if (serviceTitle.length>25 || serviceTitle==''){
+        errors.serviceTitle = 'Title can not be more than 25 symbols';
+    }
 
-    // ვალიდაცია სერვისის აღწერაზე მაქსიმუმ 100 სიმბოლო
+    // validation service description maximum  100 symbols  
     let serviceDescription = document.querySelector('#service-description').value;
     if (serviceDescription.length>100 || serviceDescription==''){
         errors.serviceDescription = 'Description can not be more than 100 symbols';
     }
-        // ვალიდაცია ჩექბოქსზე
+
+    // validation at checkbox
     let isCheckBoxInputValid = false;
     let checkBoxDiv = document.querySelector('#checkbox-main-div');
     checkBoxDiv.querySelectorAll('input[type="checkbox"]').forEach(element => {
@@ -111,7 +114,7 @@ function validateForm() {
         if (isCheckBoxInputValid==false){
         errors.qualityChoise = 'Please Select';
     }
-    // ვალიდაცია რედიოზე
+    // validation on radio
     let isRadioInputValid = false;
     document.querySelectorAll('input[type="radio"]').forEach(element => {
         if(element.checked){
@@ -121,17 +124,34 @@ function validateForm() {
     if (isRadioInputValid==false){
         errors.socialMedia = 'Please Select';
     }
-    // ვალიდაცია მეილზე
+    // validation on mail
     let mail = document.querySelector('#email').value;
     if (mail == ''){
         errors.email = 'Email can not be empty';
     }
-     return errors;
-}
+    // validation on image
+    let files = imgInput.files
+    if (files.length > 0) {
+        let fileType = files[0].type;
+        let fileSize = files[0].size;
 
+        let maxFileSizeMb = 1000 * 1024;
+        let supportedFileTypes = ['image/png','image/jpeg'];
+
+        if(!supportedFileTypes.includes(fileType)){
+            errors.myfile = 'Please select only jpg or png file type';
+        }
+
+        if (fileSize > maxFileSizeMb) {
+            errors.myfile = 'Please select image size less than 1 MB';
+        }
+    }
+    return errors;
+};
 
 document.querySelector('#service-form').addEventListener('submit', onServiceFormSubmit);
 
+// this function valisate form and if there are no mistakes adds it, after this reset error spans
 function onServiceFormSubmit(event){
     event.preventDefault();
     let errors = validateForm();
@@ -145,12 +165,14 @@ function onServiceFormSubmit(event){
     }
 }
 
+// this function resets error span texts after opening a service form
 function resetErrors() {
     document.querySelectorAll('.span-error').forEach(item => {
         item.innerHTML = '';
     });
 }
 
+// this function collerct error texts
 function displayErrors(errors) {
     for (let item in errors) {
         let errorSpan = document.getElementById('error_' + item);
@@ -160,6 +182,7 @@ function displayErrors(errors) {
     }
 }
 
+// this function validate email in live
 function validation() {
     let emailText = document.getElementById('email').value;
     let spanText = document.getElementById('error_email'); 
@@ -174,7 +197,7 @@ function validation() {
     }
 }
 
-// burger bar
+// create burger bar
 let navigation = document.getElementById('nav-block');
 let burgerButton = document.getElementById('burger-bar');
 let topChild = document.getElementById('top-child');
@@ -187,3 +210,11 @@ burgerButton.addEventListener('click', function(){
     middleChild.classList.toggle('middle');
     bottomChild.classList.toggle('bottom');
 });
+
+// This is current date
+function currentDate(){
+    var today = new Date(); 
+    var date = document.getElementById('current-date');
+    date.innerHTML = today;
+}
+currentDate();
